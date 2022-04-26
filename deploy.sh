3f45744e7e572f9f4d6e722efe607ae72e4db23d
Divyeshzb <60085094+Divyeshzb@users.noreply.github.com>
2022-04-26 13:29:35 +0530

    Minor change in script file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-cd $ROOST_WORK_DIR/git/Votingapp
+# cd $ROOST_WORK_DIR/git/Votingapp
 ns=$ROOST_NAMESPACE
 
 helm install -g helm-vote -n $ns --set namespace=$ns,clusterIP=$ROOST_CLUSTER_IP,tag=$ROOST_ENV_ID --create-namespace
